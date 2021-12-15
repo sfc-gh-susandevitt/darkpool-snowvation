367c9ae889af508d062af3207c682a54dee29eb3
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-15 18:30:28 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -79,7 +79,7 @@ def init_connection():
     return snowflake.connector.connect(**st.secrets["snowflake"])
 
 conn = init_connection()
-run_the_query = st.sidebar.button("Connect to your datasets")
+#run_the_query = st.sidebar.button("Connect to your datasets")
 
 # Perform query.
 # Uses st.cache to only rerun when the query changes or after 10 min.
@@ -102,7 +102,7 @@ def run_query(query):
 #database = run_query("select TABLE_NAME from DEMAND.INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA not in ('INFORMATION_SCHEMA');")
 #st.write(database)
 
-if run_the_query:
+#if run_the_query:
     run_query("select concat(TABLE_CATALOG,'.',TABLE_SCHEMA,'.',TABLE_NAME) from DEMAND.INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA not in ('INFORMATION_SCHEMA');")
 
 # Print results.
