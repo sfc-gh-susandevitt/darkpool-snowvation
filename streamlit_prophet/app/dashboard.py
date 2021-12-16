c3ec9c687800b0019c7d90ba18b1d7fae06e08c9
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-16 10:19:20 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -161,7 +161,7 @@ if analyze==True:
             job=df["TRAINING_JOB"].astype(str)
             acc=df["INCREASED_ACCURACY"].astype(str)
             source=df
-            base=alt.CHART(source).encode(x=job)
+            base=alt.Chart(source).encode(x=job)
             bar=base.mark_bar().encode(y=auc)
             line=base.mark_line(color='red').encode(y=acc)
             (bar + line).properties(width=600)
