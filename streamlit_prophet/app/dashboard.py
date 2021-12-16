568010e50b86919adfdf501ed2848ebf72c1865f
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-15 21:38:27 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -140,6 +140,8 @@ if analyze=='On':
             # Return a Pandas DataFrame containing all of the results.
             df = cur.fetch_pandas_all()
             st.dataframe(df)
+            chart_data = (df[['TRAINING_JOB','AUC','INCREASED_ACCURACY']])
+            st.bar_chart(chart_data)
 if analyze=='Off':
     def run_query(query):
         with conn.cursor() as cur:
@@ -166,8 +168,6 @@ if boost=='On':
             # Return a Pandas DataFrame containing all of the results.
             df = cur.fetch_pandas_all()
             st.dataframe(df)
-            chart_data = (df[['TRAINING_JOB','AUC','INCREASED_ACCURACY']])
-            st.bar_chart(chart_data)
 if boost=='Off':
     def run_query(query):
         with conn.cursor() as cur:
