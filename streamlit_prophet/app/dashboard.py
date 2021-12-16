14bbc712fcb17f84136201f1ab0abe22e558076b
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-16 13:13:40 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -200,18 +200,29 @@ if boost==True:
             cur.execute(query_text)      
             df = cur.fetch_pandas_all()
             option = st.selectbox('Select your dataset for inference', df)
+            if st.button('Run Inference'):
+                def run_query(query_text2):
+                 with conn.cursor() as cur:
+                    cur.execute(query_text2)      
+                    df = cur.fetch_pandas_all()
+                    st.write(df)
+
+#     run_query("select * from darkpool_common.ml.demand1_scoring_output limit 20;")  
 if boost==False:
      def run_query(query):
         with conn.cursor() as cur:
             cur.execute(query)         
-        
+
+            
+            
+            
 run_query("select concat(TABLE_CATALOG,'.',TABLE_SCHEMA,'.',TABLE_NAME) from DEMAND1.INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA in ('PUBLIC');")        
     
-if st.button('Run Inference'):
-    def run_query(query_text2):
-      with conn.cursor() as cur:
-        cur.execute(query_text2)      
-        df = cur.fetch_pandas_all()
-        st.write(df)
+# if st.button('Run Inference'):
+#     def run_query(query_text2):
+#       with conn.cursor() as cur:
+#         cur.execute(query_text2)      
+#         df = cur.fetch_pandas_all()
+#         st.write(df)
 
-    run_query("select * from darkpool_common.ml.demand1_scoring_output limit 20;")            
+#     run_query("select * from darkpool_common.ml.demand1_scoring_output limit 20;")            
