947f8446e711d8a95c58256e210c20cfb1ff7758
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-16 13:20:05 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -209,11 +209,11 @@ if boost==False:
             
 run_query("select concat(TABLE_CATALOG,'.',TABLE_SCHEMA,'.',TABLE_NAME) from DEMAND1.INFORMATION_SCHEMA.TABLES where TABLE_SCHEMA in ('PUBLIC');")        
     
-if st.button('Run Inference'):
-    def run_query(query_text2):
-      with conn.cursor() as cur:
-        cur.execute(query_text2)      
-        df = cur.fetch_pandas_all()
-        st.write(df)
+# if st.button('Run Inference'):
+#     def run_query(query_text2):
+#       with conn.cursor() as cur:
+#         cur.execute(query_text2)      
+#         df = cur.fetch_pandas_all()
+#         st.write(df)
 
-    run_query("select * from darkpool_common.ml.demand1_scoring_output limit 20;")            
+#     run_query("select * from darkpool_common.ml.demand1_scoring_output limit 20;")            
