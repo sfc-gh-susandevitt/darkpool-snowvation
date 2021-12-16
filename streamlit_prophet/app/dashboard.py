752a7aa3300557ab76b777c7d579d9139e33de08
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-16 13:26:26 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -212,6 +212,7 @@ if st.button('Run Inference'):
       with conn.cursor() as cur:
         cur.execute(query_text2)      
         df = cur.fetch_pandas_all()
+        st.write("You have paid $277 for at 27.7 point accuracy boost in your inference model.  See a sample of your inferenced data here:"
         st.write(df)
 
     run_query("select * from darkpool_common.ml.demand1_scoring_output limit 20;")            
