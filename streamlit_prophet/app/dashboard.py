f013d5364ad74c8a4d545f323b35c1c4be5216da
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-16 00:06:27 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -195,8 +195,8 @@ if boost==True:
             # Return a Pandas DataFrame containing all of the results.
             df = cur.fetch_pandas_all()
             st.dataframe(df)
-#if st.session_state.key==True:
-#   st.balloons()
+if st.session_state.key==True:
+   st.balloons()
 if boost==False:
     def run_query(query):
         with conn.cursor() as cur:
