91ba33923654b984850158540d354120b185c246
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-15 22:44:03 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -59,7 +59,7 @@ dates: Dict[Any, Any] = dict()
 report: List[Dict[str, Any]] = []
 
 # Info
-with st.expander("*What is darkpool*?", expanded=True):
+with st.expander("What is darkpool?", expanded=False):
     st.write(readme["app"]["app_intro"])
     st.write("")
 st.write("")
