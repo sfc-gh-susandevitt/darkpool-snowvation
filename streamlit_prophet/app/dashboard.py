bfada58b840911b3847b6cb5f53701fed3214198
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-15 20:15:46 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -134,10 +134,10 @@ run_query(query_text)
 
 st.subheader("Analyze Potential Boost?")
 st.write("")
-st.button("Yes, Analyze Boost")
+st.button("Yes")
 
-if st.button ('Yes, Analyze Boost'):
-    st.write("Potential Accuracy Boost for Your Dataset")
+if st.button ('Yes'):
+    st.write('Potential Accuracy Boost for Your Dataset')
     st.write("")
 
 st.header("Auto-boost your models?")
