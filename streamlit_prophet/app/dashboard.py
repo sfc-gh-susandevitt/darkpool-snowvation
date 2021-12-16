390c58800a99e206808baf813f50213f29d43096
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-15 23:40:32 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -162,7 +162,8 @@ if pricing==True:
             df = cur.fetch_pandas_all()
             col1,col2,col3 = st.columns(3)
             #col1.metric("Increased Accuracy", "47%")
-            col1.metric("Increased Accuracy", df['INCREASED_ACCURACY'])
+            accuracy = df['INCREASED_ACCURACY']
+            col1.metric("Increased Accuracy",value = accuracy )
             col2.metric("Rows","1,000,023")
             col3.metric("Price","$1,000")
             st.dataframe(df)
