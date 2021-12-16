6ca657e6d52d18e325418ae794a7de4c14175a4b
rachelblumsnow <68304022+rachelblumsnow@users.noreply.github.com>
2021-12-16 17:08:57 -0500

    Update dashboard.py

--- streamlit_prophet/app/dashboard.py
+++ streamlit_prophet/app/dashboard.py
@@ -50,7 +50,7 @@ from streamlit_prophet.lib.models.prophet import forecast_workflow
 from streamlit_prophet.lib.utils.load import load_config, load_image
 
 # Page config
-st.set_page_config(page_title="darkpool")
+st.set_page_config(page_title="darkpool", favicon=st.image(load_image("Darkpoolwhite.png")))
 
 # Load config
 config, instructions, readme = load_config(
